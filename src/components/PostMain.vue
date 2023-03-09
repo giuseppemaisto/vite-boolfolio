263a57a7d849d250c57dcc5905b7f8cc43096070
giuseppemaisto <gmaisto98@gmail.com>
2023-03-09 15:05:02 +0100

    milestone 3 completa :creato progetto e installato axios

---
+++ src/components/PostMain.vue
@@ -0,0 +1,24 @@
+<script>
+import axios from 'axios';
+export default {
+    name: "PostMain",
+    data(){
+        return{
+            posts: [],
+            loading: true,
+            BaseUrl:"http://127.0.0.1:8000"
+        }
+    }
+};
+</script>
+
+<template>
+  <div>
+
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
