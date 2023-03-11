17cb7c258b73937603334ee42677b38e96f5106e
giuseppemaisto <gmaisto98@gmail.com>
2023-03-12 00:42:44 +0100

    inizio dettaglio pagina

---
+++ src/pages/SinglePost.vue
@@ -0,0 +1,16 @@
+        <script>
+        export default {
+            name: 'SinglePost',
+            data(){
+                return
+            }
+        }
+        </script>
+<template lang="">
+    <div>
+        <h2>dettaglio post</h2>
+    </div>
+</template>
+<style lang="">
+    
+</style>
--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './pages/HomePage.vue';
 import PostList from './pages/PostList.vue';
+import SinglePost from './pages/SinglePost.vue'
+
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -14,7 +16,14 @@ const router = createRouter({
             path: '/progetti',
             name: 'post_list',
             component: PostList
-        }
+        },
+        //rotta dettaglio post 
+       {
+            path:'/prodotti/:slug',//il nome del parametro deve conciderre con qeullo dato in api.php di laravel
+            name:'single_post',
+            component: SinglePost
+
+       }
     ]
 }); 
 export { router };
