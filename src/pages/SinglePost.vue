7766f9023bac684388fc56f9a4b0809bfd19d44c
giuseppemaisto <gmaisto98@gmail.com>
2023-03-13 23:23:00 +0100

    pagina dettagli fatta

--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -29,17 +29,28 @@
         <template lang="">
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 d-flex justify-content-between my-2">
                     <h2>{{post.title}}</h2>
+                    <router-link :to="{name: 'post_list'}" class="btn btn-success">torna agli articoli </router-link>
                 </div>
                 <div class="cover_img">
-                     <img :src="`${this.store.BaseUrl}/storage/${this.post.cover_image}`" class="img-fluid" alt="">
+                     <img :src="post.cover_image != null ? `${this.store.BaseUrl}/storage/${this.post.cover_image}` : 'https://picsum.photos/300/200'" class="img-fluid" alt="">
                 </div>
                 <div>
                     <p>
                         {{post.description}}
                     </p>
                 </div>
+                <div class="d-flex">
+                    <strong>categoria:  </strong>
+                    <p class="" v-if="post.type">
+                        {{ post.type.name }}
+                    </p>
+                    <p v-else>
+                        nessuna categoria selezionata
+                    </p>
+                </div>
+
             </div>
             
         </div>
@@ -47,6 +58,6 @@
   
      <style lang="scss" scoped>
         .cover_img{
-            width: 200px;
+            width: 300px;
         }
      </style>
